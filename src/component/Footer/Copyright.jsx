879ea654a0529de7ac2ace6db2c9b07d6f9d2fb3
Ian Yin <yin.ian093@gmail.com>
2020-08-12 11:26:03 +1000

    restructure

--- src/component/Footer/Copyright.jsx
+++ src/component/Footer/Copyright.jsx
@@ -4,8 +4,8 @@ const Copyright = () => (
     <div>
         <ul>
             <li>Copyright 2020</li>
-            <li><a>Terms</a></li>
-            <li><a>Privacy</a></li>
+            <li><a href="www.google.com">Terms</a></li>
+            <li><a href="www.google.com">Privacy</a></li>
         </ul>
 
     </div>
