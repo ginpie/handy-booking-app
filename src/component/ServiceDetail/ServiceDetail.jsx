56ef058eb482883e0b61e489f2d44c9e46893e1c
Ian Yin <yin.ian093@gmail.com>
2020-08-20 12:33:29 +1000

    add some style

--- src/component/ServiceDetail/ServiceDetail.jsx
+++ src/component/ServiceDetail/ServiceDetail.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 import Order from './component/Order';
 import Profile from './component/Profile';
 
-import style from './ServiceDetail.module.css';
+import style from './ServiceDetail.module.scss';
 
-const ServiceDetail = () => (
-    <div className={style.page}>
-        <Profile />
-        <Order>Furniture Assembly</Order>
-    </div>
-);
+class ServiceDetail extends React.Component {
+    constructor(props) {
+        super(props);
+        this.order = "Furniture Assembly";
+        this.state = {
+            order : this.order,
+        }
+    }
+
+    render() {
+        const order = this.state.order;
+        return (
+            <div className={style.page}>
+                <div className={style.title}>
+                    <p>{order}</p>
+                </div>
+                <Profile />
+                <Order>{order}</Order>
+            </div>
+        )
+    }
+}
 
 export default ServiceDetail;
--- src/component/ServiceDetail/component/Profile/component/InformationPair.jsx
+++ src/component/ServiceDetail/component/Profile/component/InformationPair.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import style from '../Profile.module.css';
 
 const InformationPair = (props) => (
     <tr>
