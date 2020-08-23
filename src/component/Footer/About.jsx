639a35594c76acc6cfc77ddd26869b12b6cf03c7
chen <chenjp1989@gmail.com>
2020-08-23 15:42:24 +1000

    init commit

---
+++ src/component/Footer/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <ul>
+      <li>
+        <a href="www.google.com">About</a>
+      </li>
+      <li>
+        <a href="www.google.com">Contact</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default About;
