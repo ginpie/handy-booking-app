f527d81a01d7452708becfafba0376920b14abea
chen <chenjp1989@gmail.com>
2020-09-08 01:20:37 +1000

    update search bar to new version

--- src/component/SearchBar/SearchBar.js
+++ src/component/SearchBar/SearchBar.js
@@ -6,21 +6,38 @@ class SearchBar extends Component {
     return (
       <section className="search-bar">
         <div className="search-bar__title-box">
-          <h2 className="search-bar__title">Search Your Local Family Help</h2>
+          <h2 className="search-bar__title">NEED A HAND?</h2>
+          <h3 className="search-bar__subtitle">
+            The #1 tradie service in Australia
+          </h3>
         </div>
         <div className="search-bar__container">
           <form className="form" id="addItemForm">
-            <input
-              type="text"
-              className="input"
-              id="addInput"
-              placeholder="Search by business name..."
-            />
+            <div className="input-box input-box--name">
+              <i class="fas fa-search"></i>
+              <input
+                type="text"
+                className="input"
+                id="addInputName"
+                placeholder="Search by trade or business name"
+              />
+            </div>
+            <div className="input-box input-box--address">
+              <i class="fas fa-map-marker-alt"></i>
+              <input
+                type="text"
+                className="input"
+                id="addInputAddress"
+                placeholder="Enter postcode"
+              />
+            </div>
             <button className="button" onClick={"" /* search function here */}>
-              Search
+              Go
             </button>
           </form>
         </div>
+
+        <button className="get-started">GET STARTED</button>
       </section>
     );
   }
