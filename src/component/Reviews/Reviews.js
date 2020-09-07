08a4d40fbdaee92b347c9573933d86dbe55fb4f3
chen <chenjp1989@gmail.com>
2020-09-08 01:19:05 +1000

    create reviews section

---
+++ src/component/Reviews/Reviews.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./reviews.css";
+
+const Reviews = () => (
+  <section className="reviews">
+    <div className="reviews__title-box">
+      <h3 className="reviews__title">SEE WHAT THE OTHERS ARE GETTING DONE</h3>
+    </div>
+  </section>
+);
+
+export default Reviews;
