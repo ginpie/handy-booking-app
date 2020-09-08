0fd75f1a2984128526b35f7c465ae8dc40273ea8
Mingxin Dong <836514519dmx@gmail.com>
2020-09-08 10:55:53 +1000

    commit

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -3,20 +3,15 @@ import React from 'react';
 // import './App.css';
 import Header from '../Header';
 import Footer from '../Footer';
-//need delete
-import UserProfile from '../UserProfile/index';
 
 
 function App() {
   return (
-    // <div className="App">
-    //   <Header />
+    <div className="App">
+      <Header />
 
-    //   <Footer />
-    // </div>
-    
-    //need delete
-    <UserProfile />
+      <Footer />
+    </div>
   );
 }
 
