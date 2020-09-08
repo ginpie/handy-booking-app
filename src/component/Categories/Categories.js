34037cf8d503fdb2ba13a1e1c482a892dd6cfead
chen <chenjp1989@gmail.com>
2020-09-08 23:11:29 +1000

    fix minor bug

--- src/component/Categories/Categories.js
+++ src/component/Categories/Categories.js
@@ -10,43 +10,43 @@ const Categories = () => (
     <div className="categories__title-box">
       <h3 className="categories__title">WHAT WE OFFER?</h3>
     </div>
-    <div class="carousel" data-flickity='{ "autoPlay": true }'>
-      <div class="carousel-cell">
+    <div className="carousel" data-flickity='{ "autoPlay": true }'>
+      <div className="carousel-cell">
         <div className="cell-wrapper">
           <a href="www.google.com">
-            <i class="fas fa-hand-sparkles"></i>
+            <i className="fas fa-hand-sparkles"></i>
             <p className="carousel-cell__job-title">Home Cleaning</p>
           </a>
         </div>
       </div>
-      <div class="carousel-cell">
+      <div className="carousel-cell">
         <div className="cell-wrapper">
           <a href="www.google.com">
-            <i class="fas fa-truck"></i>
+            <i className="fas fa-truck"></i>
             <p className="carousel-cell__job-title">Full House Removal</p>
           </a>
         </div>
       </div>
-      <div class="carousel-cell">
+      <div className="carousel-cell">
         <div className="cell-wrapper">
           <a href="www.google.com">
-            <i class="fas fa-tools"></i>
+            <i className="fas fa-tools"></i>
             <p className="carousel-cell__job-title">Handy Man</p>
           </a>
         </div>
       </div>
-      <div class="carousel-cell">
+      <div className="carousel-cell">
         <div className="cell-wrapper">
           <a href="www.google.com">
-            <i class="fas fa-couch"></i>
+            <i className="fas fa-couch"></i>
             <p className="carousel-cell__job-title">Furniture Assembly</p>
           </a>
         </div>
       </div>
-      <div class="carousel-cell">
+      <div className="carousel-cell">
         <div className="cell-wrapper">
           <a href="www.google.com">
-            <i class="fas fa-hammer"></i>
+            <i className="fas fa-hammer"></i>
             <p className="carousel-cell__job-title">Installation</p>
           </a>
         </div>
