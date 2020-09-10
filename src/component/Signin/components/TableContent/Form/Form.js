11ab483245d99506b59a1c80a6abff3f002a7f9c
Hunterprey <595129026@qq.com>
2020-09-10 22:14:11 +1000

    improve reusable

--- src/component/Signin/components/TableContent/Form/Form.js
+++ src/component/Signin/components/TableContent/Form/Form.js
@@ -1,40 +1,23 @@
 import React from "react";
 import Styles from "./Form.module.css";
-import { Formik } from "formik";
+import { Formik, Form, useField } from "formik";
 import * as Yup from "yup";
-// const validate = (values) => {
-//   const errors = {};
 
-//   if (!values.email) {
-//     errors.email = "Required";
-//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-//     errors.email = "Invalid email address";
-//   }
-//   if (!values.password) {
-//     errors.password = "Required";
-//   } else if (values.password.length > 20) {
-//     errors.password = "Must be 20 characters or less";
-//   }
-//   if (!values.confirmpassword) {
-//     errors.confirmpassword = "Required";
-//   } else if (values.confirmpassword !== values.password) {
-//     errors.password = "Not Same";
-//   }
-//   return errors;
-// };
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name} className={Styles.label}>
+        {label}
+      </label>
+      <input className={Styles.input} {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className={Styles.errorText}>{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 const NameForm = () => {
-  // const formik = useFormik({
-  //   initialValues: {
-  //     email: "",
-  //     password: "",
-  //     confirmpassword: "",
-  //   },
-  //   validate,
-  //   onSubmit: (values) => {
-  //     alert(JSON.stringify(values, null, 2));
-  //   },
-  // });
-
   return (
     <Formik
       initialValues={{ email: "", password: "", confirmpassword: "" }}
@@ -60,63 +43,81 @@ const NameForm = () => {
         }, 400);
       }}
     >
-      {(formik) => (
-        <form onSubmit={formik.handleSubmit}>
-          <div className={Styles.layout}>
-            <label className={Styles.label}>Email:</label>
-            <input
-              id="email"
-              // name="email"
-              type="email"
-              className={Styles.input}
-              // value={formik.values.email}
-              // onChange={formik.handleChange}
-              // onBlur={formik.handleBlur}
-              {...formik.getFieldProps("email")}
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <div className={Styles.errorText}>{formik.errors.email}</div>
-            ) : null}
-            <label className={Styles.label}>Password :</label>
-            <input
-              id="password"
-              // name="password"
-              type="text"
-              className={Styles.input}
-              // value={formik.values.password}
-              // onChange={formik.handleChange}
-              // onBlur={formik.handleBlur}
-              {...formik.getFieldProps("password")}
-            />
-            {formik.touched.password && formik.errors.password ? (
-              <div className={Styles.errorText}>{formik.errors.password}</div>
-            ) : null}
-            <label className={Styles.label}>Confirm-Password :</label>
-            <input
-              id="confirmpassword"
-              // name="confirmpassword"
-              type="text"
-              className={Styles.input}
-              // value={formik.values.confirmpassword}
-              // onChange={formik.handleChange}
-              // onBlur={formik.handleBlur}
-              {...formik.getFieldProps("confirmpassword")}
-            />
-            {formik.touched.confirmpassword && formik.errors.confirmpassword ? (
-              <div className={Styles.errorText}>
-                {formik.errors.confirmpassword}
-              </div>
-            ) : null}
-
-            {/* <button className={Styles.forget}>Forget Password ?</button> */}
-            <input
-              className={Styles.submit}
-              type="submit"
-              value="Join XXXXXXXXX"
-            />
-          </div>
-        </form>
-      )}
+      <Form>
+        <div className={Styles.layout}>
+          {/* <label className={Styles.label}>Email:</label> */}
+          {/* <Field
+            name="email"
+            // name="email"
+            // type="email"
+            // className={Styles.input}
+            // value={formik.values.email}
+            // onChange={formik.handleChange}
+            // onBlur={formik.handleBlur}
+            // {...formik.getFieldProps("email")}
+          /> */}
+          {/* <ErrorMessage name="email" /> */}
+          {/* {formik.touched.email && formik.errors.email ? (
+            <div className={Styles.errorText}>{formik.errors.email}</div>
+          ) : null} */}
+          <MyTextInput
+            label="Email Address"
+            name="email"
+            type="email"
+            placeholder="jane@formik.com"
+          />
+          <MyTextInput
+            label="Password"
+            name="password"
+            type="password"
+            placeholder=""
+          />
+          <MyTextInput
+            label="Confirm Password"
+            name="confirmpassword"
+            type="password"
+          />
+          {/* <label className={Styles.label}>Password :</label>
+          <Field
+            name="password"
+            // name="password"
+            type="text"
+            className={Styles.input}
+            // value={formik.values.password}
+            // onChange={formik.handleChange}
+            // onBlur={formik.handleBlur}
+            //   ||
+            // {...formik.getFieldProps("password")}
+          /> */}
+          {/* {formik.touched.password && formik.errors.password ? (
+            <div className={Styles.errorText}>{formik.errors.password}</div>
+          ) : null} */}
+          {/* <ErrorMessage name="password" />
+          <label className={Styles.label}>Confirm-Password :</label>
+          <Field
+            name="confirmpassword"
+            // name="confirmpassword"
+            type="text"
+            className={Styles.input}
+            // value={formik.values.confirmpassword}
+            // onChange={formik.handleChange}
+            // onBlur={formik.handleBlur}
+            // {...formik.getFieldProps("confirmpassword")}
+          />
+          {/* {formik.touched.confirmpassword && formik.errors.confirmpassword ? (
+            <div className={Styles.errorText}>
+              {formik.errors.confirmpassword}
+            </div>
+          ) : null} */}
+          {/* <ErrorMessage name="confirmpassword" className="errorText" /> */}
+          {/* <button className={Styles.forget}>Forget Password ?</button> */}
+          <input
+            className={Styles.submit}
+            type="submit"
+            value="Join XXXXXXXXX"
+          />
+        </div>
+      </Form>
     </Formik>
   );
 };
