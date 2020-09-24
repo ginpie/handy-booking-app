4cbce655521a9655efe8eea2481e2e4f956b147a
Hunterprey <595129026@qq.com>
2020-09-25 00:49:17 +1000

    remove database from front side to the backend side

--- src/db/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports=(error, req, res, next)=>{
-    if(error.name==='ValidationError'){
-        return res.status(400).json({errors:error.errors});
-    }
-    console.error(error);
-    return res.status(500).json({errors:{message:'something wrong'}});
-}
--- src/db/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const customerRoute = require("./routes/customers");
-
-router.use("/customers", customerRoute);
-module.exports = router;
