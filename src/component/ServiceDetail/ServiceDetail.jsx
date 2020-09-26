071941f3f49208b07b14b3f10965620ab4946e78
IANY <yin.ian093@gmail.com>
2020-09-27 00:53:03 +1000

    test

---
+++ src/component/ServiceDetail/ServiceDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+`;
+
+const ServiceDetail = () => (
+    <Layout></Layout>
+);
+
+export default ServiceDetail;
