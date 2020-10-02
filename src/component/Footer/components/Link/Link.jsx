b55a6955419f8132909c3dc6d7e93a1f46562fa3
chen <chenjp1989@gmail.com>
2020-10-02 17:44:03 +1000

    add Link components to footer

---
+++ src/component/Footer/components/Link/Link.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Link = styled.a`
+  text-decoration: none;
+  color: #fff;
+  display: inline-block;
+  margin: 0 0 20px 0;
+`;
+
+export default Link;
