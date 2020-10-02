576b375cb20e9106ac6a0399bcd49fa922278c46
chen <chenjp1989@gmail.com>
2020-10-02 17:43:44 +1000

    delete due to structure change

--- src/component/Tradies/dist/tradies.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-require("./tradies.css");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
