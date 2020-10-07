0c01cd357dbd7d1b6263d2f698691706997201fd
chen <chenjp1989@gmail.com>
2020-10-07 17:37:43 +1100

    add page router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import "./index.css";
 import App from "./component/App";
 import "normalize.css";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import JoinUsPage from "./component/JoinUs";
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/join-us" component={JoinUsPage} />
+    </Switch>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
