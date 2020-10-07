e049159debafc915c2895b102cf8383c9934dd1b
chen <chenjp1989@gmail.com>
2020-10-07 14:38:06 +1100

    minor bug fix

--- src/component/Categories/components/Carousel/Carousel.jsx
+++ src/component/Categories/components/Carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.p`
+const Wrapper = styled.div`
   width: 150px;
 `;
 
--- src/component/Tradies/Tradies.jsx
+++ src/component/Tradies/Tradies.jsx
@@ -44,7 +44,7 @@ const Tradies = () => (
     <Tab></Tab>
     <TradieCard
       title="Alice"
-      num="5"
+      num={5}
       img={p4}
       content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     ></TradieCard>
