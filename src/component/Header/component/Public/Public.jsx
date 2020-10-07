6ef2ec3e74e10066cd74702557e8e5124579f6de
chen <chenjp1989@gmail.com>
2020-10-07 18:27:11 +1100

    fix home link

--- src/component/Header/component/Public/Public.jsx
+++ src/component/Header/component/Public/Public.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import LinkItem from '../LinkItem';
-import Layout from '../Layout';
-import CategoriesDropdown from './component/CategoriesDropdown';
+import React from "react";
+import LinkItem from "../LinkItem";
+import Layout from "../Layout";
+import CategoriesDropdown from "./component/CategoriesDropdown";
 
 const Public = () => (
-    <Layout>
-        <LinkItem href="/home" linkType={"nav"}>Home</LinkItem>
-        <CategoriesDropdown />
-        <LinkItem href="/contact" linkType={"nav"}>Contact</LinkItem>
-    </Layout>
+  <Layout>
+    <LinkItem href="/" linkType={"nav"}>
+      Home
+    </LinkItem>
+    <CategoriesDropdown />
+    <LinkItem href="/contact" linkType={"nav"}>
+      Contact
+    </LinkItem>
+  </Layout>
 );
 
 export default Public;
