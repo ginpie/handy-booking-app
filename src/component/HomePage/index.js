ba094acde11a314a41f4809bb6eacda422669c08
chen <chenjp1989@gmail.com>
2020-10-08 15:51:15 +1100

    initial commit

---
+++ src/component/HomePage/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Header from "../Header";
+import Footer from "../Footer";
+import Categories from "../Categories/Categories";
+import SearchBar from "../SearchBar/SearchBar";
+import Reviews from "../Reviews/Reviews";
+import Tradies from "../Tradies/Tradies";
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <SearchBar />
+      <Categories />
+      <Reviews />
+      <Tradies />
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
