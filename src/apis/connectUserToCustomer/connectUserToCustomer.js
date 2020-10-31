de6cd9ed18feb4aaed558f8e6e04022adec33d6f
Hunterprey <595129026@qq.com>
2020-11-01 00:17:10 +1000

    update Connect /fix

--- src/apis/connectUserToCustomer/connectUserToCustomer.js
+++ src/apis/connectUserToCustomer/connectUserToCustomer.js
@@ -1,10 +1,9 @@
 import api from '../../lib/api';
 const connectUserToCustomer = ( email ) =>{
+
     api.get('/auth')
     .then((response) => {
         const id = response.data.id
-        console.log(response.data.id)    
-        console.log(email)
         api.post(`/users/${id}/customers/${email}`)
     })
 }
