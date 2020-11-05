eb90dfcbcd0a66f2ffc49e407242f4998f6ec36c
chen <chenjp1989@gmail.com>
2020-11-05 23:53:47 +1100

    update api url key

--- src/apis/createInquiry/createInquiry.js
+++ src/apis/createInquiry/createInquiry.js
@@ -1,14 +1,14 @@
-import api from '../../lib/api';
-const createInquiry = ( data ) =>{
-    api.post('/inquiry',{data})
-    // .then((inquiryID)=>{
-    //     const code =inquiryID.data.id
-    //     api.get('/auth')
-    //     return code
-    // })
-    // .then((result)=>{
-    //     const email = result.data.email
-    //     api.post(`/customers/${email}/inquiry/${code}`)
-    // })gi
-}
+import { instance as api } from "../../lib/api";
+const createInquiry = (data) => {
+  api.post("/inquiry", { data });
+  // .then((inquiryID)=>{
+  //     const code =inquiryID.data.id
+  //     api.get('/auth')
+  //     return code
+  // })
+  // .then((result)=>{
+  //     const email = result.data.email
+  //     api.post(`/customers/${email}/inquiry/${code}`)
+  // })gi
+};
 export default createInquiry;
