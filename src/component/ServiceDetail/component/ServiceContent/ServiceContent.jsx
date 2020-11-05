b8460caef047929790955e8306be3b1aac162346
chen <chenjp1989@gmail.com>
2020-11-05 23:54:10 +1100

    update service order content

--- src/component/ServiceDetail/component/ServiceContent/ServiceContent.jsx
+++ src/component/ServiceDetail/component/ServiceContent/ServiceContent.jsx
@@ -1,92 +1,113 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
-import CardWrapper from './component/CardWrapper';
-import OrderLayout from './component/OrderLayout';
-import { getTradies } from '../../../../apis/getTradies/getTradies';
+import React from "react";
+import styled, { css } from "styled-components";
+import CardWrapper from "./component/CardWrapper";
+import OrderLayout from "./component/OrderLayout";
+import { getTradies } from "../../../../apis/getTradies/getTradies";
 
-const Wrapper = styled.div`
-`;
+const Wrapper = styled.div``;
 
 const Layout = styled.div`
-    padding: 30px;
-    max-width: 1024px;
-    margin: 0 auto;
+  padding: 30px;
+  max-width: 1024px;
+  margin: 0 auto;
 `;
 
 const Header = styled.h1`
-    font-size: 1.2rem;
+  font-size: 1.2rem;
 `;
 
 const Content = styled.div`
-    display: flex;
+  display: flex;
 
-    ${props => props.isClicked && css`
-        width: 260px;
+  ${(props) =>
+    props.isClicked &&
+    css`
+      width: 260px;
     `}
 `;
 
 const data = [
-    {name: "Ian Yin", label: ["Cleaning", "Furniture Assembly", "House Moving", "Installation"], rating: 3},
-    {name: "David", label: ["Cleaning", "Furniture Assembly", "House Moving"], rating: 5},
-    {name: "Jinpei", label: ["Cleaning", "Furniture Assembly", "House Moving"], rating: 4},
-    {name: "Ming", label: ["Cleaning", "Furniture Assembly"], rating: 5},
-    {name: "katrina", label: ["Cleaning", "Furniture Assembly"], rating: 4},
-    {name: "Lightman", label: ["Cleaning", "Furniture Assembly"], rating: 4},
-    {name: "Mason", label: ["Installation"], rating: 5},
-]
+  {
+    name: "Ian Yin",
+    label: ["Cleaning", "Furniture Assembly", "House Moving", "Installation"],
+    rating: 3,
+  },
+  {
+    name: "David",
+    label: ["Cleaning", "Furniture Assembly", "House Moving"],
+    rating: 5,
+  },
+  {
+    name: "Jinpei",
+    label: ["Cleaning", "Furniture Assembly", "House Moving"],
+    rating: 4,
+  },
+  { name: "Ming", label: ["Cleaning", "Furniture Assembly"], rating: 5 },
+  { name: "katrina", label: ["Cleaning", "Furniture Assembly"], rating: 4 },
+  { name: "Lightman", label: ["Cleaning", "Furniture Assembly"], rating: 4 },
+  { name: "Mason", label: ["Installation"], rating: 5 },
+];
 
 class ServiceContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: "",
+      isClicked: false,
+      loaded: false,
+      tradiesData: null,
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            current: "",
-            isClicked: false,
-            loaded: false,
-            tradiesData: null,
-        }
+    this.handleClick = this.handleClick.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
 
-        this.handleClick = this.handleClick.bind(this);
-        this.componentDidMount = this.componentDidMount.bind(this);
-    }
+  handleClick(data, event) {
+    event.preventDefault();
+    this.setState({
+      current: data,
+      isClicked: true,
+    });
+  }
 
-    handleClick(data, event) {
-        event.preventDefault();
-        this.setState({
-            current: data,
-            isClicked: true,
-        });
-    }
+  componentDidMount() {
+    getTradies().then((res) => {
+      this.setState({
+        tradiesData: res,
+        loaded: true,
+      });
+    });
+  }
 
-    componentDidMount() {
-        getTradies().then(res => {
-            this.setState({
-                tradiesData: res,
-                loaded: true,
-            });
-        })
-    }
+  render() {
+    const title = this.props.title;
+    const current = this.state.current;
+    const isClicked = this.state.isClicked;
+    const handleClick = this.handleClick;
+    const tradiesData = this.state.tradiesData;
 
-    render() {
-        const title = this.props.title;
-        const current = this.state.current;
-        const isClicked = this.state.isClicked;
-        const handleClick = this.handleClick;
-        const tradiesData = this.state.tradiesData;
-
-        return (
-            <Wrapper>
-                <Layout>
-                    <Header>{` ${title} Recommendations:`}</Header>
-                    {this.state.loaded ? (<Content isClicked={isClicked}>
-                        <CardWrapper current={current.tradieId} data={data} handleClick={handleClick} isClicked={isClicked} tradiesData={tradiesData}/>
-                        {isClicked && <OrderLayout currentTradie={current} />}
-                    </Content>) : <p>Loading</p>}
-                </Layout>
-            </Wrapper>
-
-        )
-    }
+    return (
+      <Wrapper>
+        <Layout>
+          <Header>{` ${title} Recommendations:`}</Header>
+          {this.state.loaded ? (
+            <Content isClicked={isClicked}>
+              <CardWrapper
+                current={current.tradieId}
+                data={data}
+                handleClick={handleClick}
+                isClicked={isClicked}
+                tradiesData={tradiesData}
+              />
+              {isClicked && <OrderLayout currentTradie={current} />}
+            </Content>
+          ) : (
+            <p>Loading</p>
+          )}
+        </Layout>
+      </Wrapper>
+    );
+  }
 }
 
 export default ServiceContent;
--- src/component/ServiceDetail/component/ServiceContent/component/OrderLayout/component/OrderForm/OrderForm.jsx
+++ src/component/ServiceDetail/component/ServiceContent/component/OrderLayout/component/OrderForm/OrderForm.jsx
@@ -1,153 +1,234 @@
-import React from 'react';
-import styled from 'styled-components';
-import OrderInput from './component/OrderInput';
+import React from "react";
+import styled from "styled-components";
+import OrderInput from "./component/OrderInput";
 import withFetch from "../../../../../../../withFetch";
 import withForm from "../../../../../../../withForm";
 import compose from "../../../../../../../../utils/compose";
-import form from "./form";
-import createInquiry from '../../../../../../../../apis/createInquiry'
+// import form from "./form";
+import createInquiry from "../../../../../../../../apis/createInquiry";
 const Form = styled.form`
-    margin: 40px 0;
+  margin: 40px 0;
 `;
 
 const FormHeader = styled.h2`
-    font-size: 0.9rem;
+  font-size: 0.9rem;
 `;
 
 const InputRow = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-top: 15px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
 `;
 
 const TextArea = styled.textarea`
-    width: 100%;
-    border: 1px solid rgb(228, 228, 228);
-    border-radius: 2px;
-    background-color: rgb(249, 251, 253);
+  width: 100%;
+  border: 1px solid rgb(228, 228, 228);
+  border-radius: 2px;
+  background-color: rgb(249, 251, 253);
 `;
 
 const Label = styled.label`
-    margin-top: 15px;
-    font-size: 0.7rem;
+  margin-top: 15px;
+  font-size: 0.7rem;
 `;
 
 const SubmitWrapper = styled.div`
-    text-align: center;
-    margin-top: 30px;
+  text-align: center;
+  margin-top: 30px;
 `;
 
 const SubmitBtn = styled.input`
-    background-color: rgb(31, 169, 187);
-    width: 160px;
-    height: 40px;
-    color: white;
-    font-size: 0.8rem;
-    font-weight: bolder;
-    border: 1px transparent solid;
-    border-radius: 5px;
+  background-color: rgb(31, 169, 187);
+  width: 160px;
+  height: 40px;
+  color: white;
+  font-size: 0.8rem;
+  font-weight: bolder;
+  border: 1px transparent solid;
+  border-radius: 5px;
 `;
 
-class OrderForm extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            inquiryForm:{
-                smallFurniture: "",
-                mediumFurniture: "",
-                largeFurniture: "",
-                name: "",
-                email: "",
-                phone: "",
-                address1: "",
-                address2: "",
-                suburt: "",
-                state: "",
-                postcode: "",
-                message: "",
-            }
-        };
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(event) {
-        // return (event)=>{
-        //     event.preventDefault();
-        //     const {value} = event.target;
-        //     this.setState((preState)=>({
-        //         inquiryForm:{
-        //             ...preState.inquiryForm,
-        //             [target]:value
-        //         }
-        //     }));
-        // }
-        const target = event.target;
-        
-        const name = target.name;
-        
-        const value = target.value;
-        
-        this.setState((prevState)=>({
-            inquiryForm:{
-                ...prevState.inquiryForm,
-                [name]: value}
-          }));
-        // this.setState({ inquiryForm: { value: event.target.value } });
-      }
-    handleFormSubmit(event){
-        const { inquiryForm } = this.state;
-        // const {
-        //     fetch,
-        //   } = this.props;
-        event.preventDefault();
-        // const {address1, address2} = 
-        // const email = formData.email.value;
-        // fetch(() => createInquiry(inquiryForm)).then((id) => {
-        //     // customerSendInquiry(id)
-        //   });
-        createInquiry(inquiryForm)
-    }
-    render(){
-        return (
-        <Form onSubmit={this.handleFormSubmit}>
+class OrderForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inquiryForm: {
+        smallFurniture: "",
+        mediumFurniture: "",
+        largeFurniture: "",
+        name: "",
+        email: "",
+        phone: "",
+        address1: "",
+        address2: "",
+        suburt: "",
+        state: "",
+        postcode: "",
+        message: "",
+      },
+    };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    // return (event)=>{
+    //     event.preventDefault();
+    //     const {value} = event.target;
+    //     this.setState((preState)=>({
+    //         inquiryForm:{
+    //             ...preState.inquiryForm,
+    //             [target]:value
+    //         }
+    //     }));
+    // }
+    const target = event.target;
+
+    const name = target.name;
+
+    const value = target.value;
+
+    this.setState((prevState) => ({
+      inquiryForm: {
+        ...prevState.inquiryForm,
+        [name]: value,
+      },
+    }));
+    // this.setState({ inquiryForm: { value: event.target.value } });
+  }
+  handleFormSubmit(event) {
+    const { inquiryForm } = this.state;
+    // const {
+    //     fetch,
+    //   } = this.props;
+    event.preventDefault();
+    // const {address1, address2} =
+    // const email = formData.email.value;
+    // fetch(() => createInquiry(inquiryForm)).then((id) => {
+    //     // customerSendInquiry(id)
+    //   });
+    createInquiry(inquiryForm);
+  }
+  render() {
+    return (
+      <Form onSubmit={this.handleFormSubmit}>
         <FormHeader>Order Details</FormHeader>
         <InputRow>
-            <OrderInput name="smallFurniture" type="number"  value={this.state.inquiryForm.smallFurniture} onChange={this.handleChange}>Small Furniture:</OrderInput>
-            <OrderInput name="mediumFurniture" type="number" value={this.state.inquiryForm.mediumFurniture} onChange={this.handleChange}>Medium Furniture:</OrderInput>
-            <OrderInput name="largeFurniture" type="number" value={this.state.inquiryForm.largeFurniture} onChange={this.handleChange}>Large Furniture:</OrderInput>
+          <OrderInput
+            name="smallFurniture"
+            type="number"
+            value={this.state.inquiryForm.smallFurniture}
+            onChange={this.handleChange}
+          >
+            Small Furniture:
+          </OrderInput>
+          <OrderInput
+            name="mediumFurniture"
+            type="number"
+            value={this.state.inquiryForm.mediumFurniture}
+            onChange={this.handleChange}
+          >
+            Medium Furniture:
+          </OrderInput>
+          <OrderInput
+            name="largeFurniture"
+            type="number"
+            value={this.state.inquiryForm.largeFurniture}
+            onChange={this.handleChange}
+          >
+            Large Furniture:
+          </OrderInput>
         </InputRow>
         <InputRow>
-            <OrderInput name="name" type="text" value={this.state.inquiryForm.name} onChange={this.handleChange}>Name:</OrderInput>
-            <OrderInput name="email" type="text" value={this.state.inquiryForm.email} onChange={this.handleChange}>Email:</OrderInput>
+          <OrderInput
+            name="name"
+            type="text"
+            value={this.state.inquiryForm.name}
+            onChange={this.handleChange}
+          >
+            Name:
+          </OrderInput>
+          <OrderInput
+            name="email"
+            type="text"
+            value={this.state.inquiryForm.email}
+            onChange={this.handleChange}
+          >
+            Email:
+          </OrderInput>
         </InputRow>
         <InputRow>
-            <OrderInput name="phone" type="text" value={this.state.inquiryForm.phone} onChange={this.handleChange}>Phone No:</OrderInput>
-            <OrderInput name="address1" type="text" value={this.state.inquiryForm.address1} onChange={this.handleChange}>Address1:</OrderInput>
+          <OrderInput
+            name="phone"
+            type="text"
+            value={this.state.inquiryForm.phone}
+            onChange={this.handleChange}
+          >
+            Phone No:
+          </OrderInput>
+          <OrderInput
+            name="address1"
+            type="text"
+            value={this.state.inquiryForm.address1}
+            onChange={this.handleChange}
+          >
+            Address1:
+          </OrderInput>
         </InputRow>
         <InputRow>
-            <OrderInput name="address2" type="text" value={this.state.inquiryForm.address2} onChange={this.handleChange}>Address2:</OrderInput>
-            <OrderInput name="suburt" type="text" value={this.state.inquiryForm.suburt} onChange={this.handleChange}>Suburb:</OrderInput>
+          <OrderInput
+            name="address2"
+            type="text"
+            value={this.state.inquiryForm.address2}
+            onChange={this.handleChange}
+          >
+            Address2:
+          </OrderInput>
+          <OrderInput
+            name="suburt"
+            type="text"
+            value={this.state.inquiryForm.suburt}
+            onChange={this.handleChange}
+          >
+            Suburb:
+          </OrderInput>
         </InputRow>
         <InputRow>
-            <OrderInput name="state" type="text" value={this.state.inquiryForm.state} onChange={this.handleChange}>State:</OrderInput>
-            <OrderInput name="postcode" type="text" value={this.state.inquiryForm.postcode} onChange={this.handleChange}>Postcode:</OrderInput>
+          <OrderInput
+            name="state"
+            type="text"
+            value={this.state.inquiryForm.state}
+            onChange={this.handleChange}
+          >
+            State:
+          </OrderInput>
+          <OrderInput
+            name="postcode"
+            type="text"
+            value={this.state.inquiryForm.postcode}
+            onChange={this.handleChange}
+          >
+            Postcode:
+          </OrderInput>
         </InputRow>
-        <Label htmlFor="addition" >Additional Details</Label>
-        <TextArea name="addition"  value={this.state.inquiryForm.addition} onChange={this.handleChange} />
+        <Label htmlFor="addition">Additional Details</Label>
+        <TextArea
+          name="addition"
+          value={this.state.inquiryForm.addition}
+          onChange={this.handleChange}
+        />
         <SubmitWrapper>
-            <SubmitBtn type="submit" value="GET PRICE NOW" />
+          <SubmitBtn type="submit" value="GET PRICE NOW" />
         </SubmitWrapper>
-    </Form>
-        )
-    }
+      </Form>
+    );
+  }
 }
 
 // const EnhancedOrderForm = compose(
 //     // withForm(form),
 //     withFetch,
 //   )(OrderForm);
-  
-  
+
 // export default EnhancedOrderForm;
-  
+
 export default OrderForm;
